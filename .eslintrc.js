90b4e93212a7344855acee46ff03c55a0fe03b1e
Giovanni Armane <giovanniarmane@gmail.com>
2019-08-15 21:13:24 -0300

    Configure ESLint rule for CommonJS export

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,26 +15,28 @@ module.exports = {
     ecmaVersion: 2018,
   },
   plugins: [
-      'fp'
-    ],
-    rules: {
-      'fp/no-arguments': 'error',
-      'fp/no-class': 'error',
-      'fp/no-delete': 'error',
-      'fp/no-events': 'error',
-      'fp/no-get-set': 'error',
-      'fp/no-let': 'error',
-      'fp/no-loops': 'error',
-      'fp/no-mutating-assign': 'error',
-      'fp/no-mutating-methods': 'error',
-      'fp/no-mutation': 'error',
-      'fp/no-nil': 'error',
-      'fp/no-proxy': 'error',
-      'fp/no-rest-parameters': 'error',
-      'fp/no-this': 'error',
-      'fp/no-throw': 'error',
-      'fp/no-unused-expression': 'error',
-      'fp/no-valueof-field': 'error',
-      'no-var': 'error'
-    }
+    'fp',
+  ],
+  rules: {
+    'fp/no-arguments': 'error',
+    'fp/no-class': 'error',
+    'fp/no-delete': 'error',
+    'fp/no-events': 'error',
+    'fp/no-get-set': 'error',
+    'fp/no-let': 'error',
+    'fp/no-loops': 'error',
+    'fp/no-mutating-assign': 'error',
+    'fp/no-mutating-methods': 'error',
+    'fp/no-mutation': [ 'error', {
+      "commonjs": true,
+    }],
+    'fp/no-nil': 'error',
+    'fp/no-proxy': 'error',
+    'fp/no-rest-parameters': 'error',
+    'fp/no-this': 'error',
+    'fp/no-throw': 'error',
+    'fp/no-unused-expression': 'error',
+    'fp/no-valueof-field': 'error',
+    'no-var': 'error'
+  },
 };
