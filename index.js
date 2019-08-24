749b200847fd43eb2ff60d15d9be0a034ad17f2d
Giovanni Armane <giovanniarmane@gmail.com>
2019-08-24 04:06:20 -0300

    Syntax tweak

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-/* eslint-disable fp/no-unused-expression */
 const container = require('./src/container');
 
 const app = container.resolve('app');
 const logger = container.resolve('logger');
 
-app.fork(
-  (error) => logger.error(`[PID ${process.pid}] application failed to initialize: ${error.message}`),
-  () => logger.info(`[PID ${process.pid}] application succesfully initialized`),
+module.exports = (
+  app.fork(
+    (error) => logger.error(`[PID ${process.pid}] application failed to initialize: ${error.message}`),
+    () => logger.info(`[PID ${process.pid}] application succesfully initialized`),
+  )
 );
