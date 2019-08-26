96a51daa657699b76eaf35a17feb91b4313c8f12
Gustavo Rodarte <gustavo.rodarte@zrp.com.br>
2019-08-26 03:55:12 -0300

    Add validate method to domain

--- src/domain/Hero.module.js
+++ src/domain/Hero.module.js
@@ -2,46 +2,31 @@ const Joi = require('@hapi/joi');
 
 module.exports = ({
   EnumsEntity: { PowerLevel },
-}) => {
-  const powerLevelEnum = Object.keys(PowerLevel);
-
-  const heroSchema = Joi.object().keys({
-    name: Joi
-      .string()
-      .alphanum()
-      .required(),
-    superpower: Joi
-      .string()
-      .required(),
-    powerLevel: Joi
-      .string()
-      .valid(powerLevelEnum)
-      .required(),
-    baseOperations: Joi
-      .string(),
-  });
-
-  return {
-    validate: (hero) => {
-      // Do validation calling Joi
-      //
-      // Should return Result.OK or Result.ERROR
-    },
-
-    addHero: (hero) => {
-      // Recommendations:
-      //
-      // Maybe inject repositories here to
-      // centralize adding hero rule in domain.
-      // Not coupled because it is injected already,
-      // it only calls repo protocol, doesn't know
-      // impl.
-      //
-      // Should resolve an call to repo, resolving
-      // or rejecting accordinly.
-      //
-      // Should call validate here and validate
-      // from Villains module.
-    },
-  };
-};
+}) => ({
+  validate(heroData) {
+    const powerLevelEnum = Object.keys(PowerLevel);
+    const heroSchema = Joi.object().keys({
+      name: Joi
+        .string()
+        .alphanum()
+        .required(),
+      superpower: Joi
+        .string()
+        .required(),
+      powerLevel: Joi
+        .string()
+        .valid(powerLevelEnum)
+        .required(),
+      baseOperations: Joi
+        .string(),
+    });
+    const { error: ERROR, value: OK } = heroSchema.validate(heroData);
+    return {
+      OK,
+      ERROR,
+    };
+  },
+  canHandleTheThreat(heroData, theatLvl) {
+    return heroData.powerLevel >= theatLvl;
+  },
+});
