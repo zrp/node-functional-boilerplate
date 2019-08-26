ba5f8d2485e1f657e60fc905d6b56dc437650125
Giovanni Armane <giovanniarmane@gmail.com>
2019-08-26 13:16:26 -0300

    Define function for Hero domain vaalidation

--- src/domain/Hero.module.js
+++ src/domain/Hero.module.js
@@ -1,32 +1,36 @@
 const Joi = require('@hapi/joi');
+const Result = require('crocks/Result');
+const { isNil } = require('crocks/predicates');
+
+const { Err, Ok } = Result;
 
 module.exports = ({
   EnumsEntity: { PowerLevel },
-}) => ({
-  validate(heroData) {
-    const powerLevelEnum = Object.keys(PowerLevel);
-    const heroSchema = Joi.object().keys({
-      name: Joi
-        .string()
-        .alphanum()
-        .required(),
-      superpower: Joi
-        .string()
-        .required(),
-      powerLevel: Joi
-        .string()
-        .valid(powerLevelEnum)
-        .required(),
-      baseOperations: Joi
-        .string(),
-    });
-    const { error: ERROR, value: OK } = heroSchema.validate(heroData);
-    return {
-      OK,
-      ERROR,
-    };
-  },
-  canHandleTheThreat(heroData, theatLvl) {
-    return heroData.powerLevel >= theatLvl;
-  },
-});
+}) => {
+  const powerLevelEnum = Object.keys(PowerLevel);
+  const heroSchema = Joi.object().keys({
+    name: Joi
+      .string()
+      .alphanum()
+      .required(),
+    superpower: Joi
+      .string()
+      .required(),
+    powerLevel: Joi
+      .string()
+      .valid(powerLevelEnum)
+      .required(),
+    baseOperations: Joi
+      .string(),
+  });
+
+  return {
+    validate(hero) {
+      const { error } = heroSchema.validate(hero);
+      return isNil(error) ? Ok(hero) : Err(error);
+    },
+    canHandleTheThreat(heroData, theatLvl) {
+      return heroData.powerLevel >= theatLvl;
+    },
+  };
+};
