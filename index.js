f0e078f810b49b684cc86bebd78557c7ca58356f
Gustavo Rodarte <gustavo.rodarte@zrp.com.br>
2020-02-20 04:11:42 -0300

    Fixed aplication start

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const container = require('./src/container');
 const app = container.resolve('app');
 const logger = container.resolve('logger');
 
-module.exports = (
-  app.fork(
-    (error) => logger.error(`[PID ${process.pid}] application failed to initialize: ${error.message}`),
-    () => logger.info(`[PID ${process.pid}] application succesfully initialized`),
-  )
-);
+module.exports = app
+  .then(() => logger.info(`[PID ${process.pid}] application succesfully initialized`))
+  .catch((error) => logger.error(`[PID ${process.pid}] application failed to initialize: ${error.message}`));
