b482feaa42e75120f52b56482bfbdb5def11088c
Pedro Gryzinsky <pedro.gryzinsky@zrp.com.br>
2020-11-20 18:03:07 -0300

    style(main): spellcheck fixes for app.then()

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const container = require('./src/container');
+const container = require('src/container');
 
 const app = container.resolve('app');
 const logger = container.resolve('logger');
 
 module.exports = app
-  .then(() => logger.info(`[PID ${process.pid}] application succesfully initialized`))
-  .catch((error) => logger.error(`[PID ${process.pid}] application failed to initialize: ${error.message}`));
+  .then(() => logger.info('Application successfully initialized!'))
+  .catch((error) => logger.error(`Application failed to initialize due to ${error.message}!`));
