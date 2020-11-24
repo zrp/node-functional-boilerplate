4a5600b21c207310da08be4abbb22beb75bf670a
Gustavo Rodarte <gustavo.rodarte@zrp.com.br>
2020-11-24 18:14:05 -0300

    feat(infra/loggin/looger.js): added Winston to logger

--- config/index.js
+++ config/index.js
@@ -2,6 +2,7 @@ const env = require('env-var');
 const apollo = require('./apollo');
 const express = require('./express');
 const mongoDb = require('./mongoDb');
+const slack = require('./slack');
 
 const nodeEnv = env.get('NODE_ENV').required().asString();
 const appIdentifier = env.get('APP_IDENTIFIER').required().asString();
@@ -10,6 +11,7 @@ module.exports = {
   apollo,
   express,
   mongoDb,
+  slack,
   nodeEnv,
   appIdentifier,
 };
