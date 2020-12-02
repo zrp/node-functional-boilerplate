e638b5370269cfb566d56a382e2801555517d6f1
Gustavo Rodarte <gustavo.rodarte@zrp.com.br>
2020-12-01 22:31:52 -0300

    fix(root.router): disable contentSecurityPolicy header on development
    Helmet contenSecurityPolicy don't allow the apollo playground works, so this commit removes in dev
    env

--- src/interfaces/http/routers/root.router.js
+++ src/interfaces/http/routers/root.router.js
@@ -1,14 +1,22 @@
 const { Router } = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const Maybe = require('crocks/Maybe');
+const unit = require('crocks/helpers/unit');
+const { isDevelopment } = require('src/utils');
 
 module.exports = ({
   corsMiddleware,
   loggerMiddleware,
   v1Router,
   errorHandler,
+  config,
 }) => Router()
-  // .use(helmet())
+  .use(helmet({
+    contentSecurityPolicy: isDevelopment(Maybe.of(config.nodeEnv))
+      ? false
+      : unit(),
+  }))
   .use(bodyParser.json())
   .use(corsMiddleware())
   .use(loggerMiddleware)
