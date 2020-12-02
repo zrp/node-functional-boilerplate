631ea114ca7265500d720a871c53c8114e6260e4
emethium <nikolas.serafini@gmail.com>
2020-12-02 16:53:26 -0300

    feat(interfaces/http/resolvers): updated Hero mutations and queries exports

--- src/interfaces/http/graphQL/resolvers/mutations/heroMutations.js
+++ src/interfaces/http/graphQL/resolvers/mutations/heroMutations.js
@@ -1,8 +1,12 @@
 module.exports = ({
   createHero,
+  updateHero,
   apolloErrorHandler,
 }) => ({
   createHero: (_, hero) => createHero(hero).toPromise()
+    .then(({ result }) => result)
+    .catch(apolloErrorHandler),
+  updateHero: (_, id, hero) => updateHero(id, hero).toPromise()
     .then(({ result }) => result)
     .catch(apolloErrorHandler),
 });
--- src/interfaces/http/graphQL/resolvers/queries/heroQueries.js
+++ src/interfaces/http/graphQL/resolvers/queries/heroQueries.js
@@ -1,8 +1,13 @@
 module.exports = ({
   getAllHeroes,
+  getHero,
 }) => ({
   allHeroes: async () => {
     const result = await getAllHeroes().toPromise();
+    return result;
+  },
+  getHero: async (heroId) => {
+    const result = await getHero(heroId).toPromise();
     return result;
   },
 });
