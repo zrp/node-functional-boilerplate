527dcdd4dcce878ccf1caf3a4c4568c73ee07781
Gustavo Rodarte <gustavo.rodarte@zrp.com.br>
2020-12-01 22:16:21 -0300

    test: remove unfinished tests

--- specs/unit/app/hero/CreateHero.spec.js
+++
@@ -1,9 +0,0 @@
-const CreateHero = require('src/app/hero/CreateHero');
-
-describe('App :: hero :: CreateHero', () => {
-  describe('when hero is valid', () => {
-    test('should create the Hero and returns a Ok output', () => {
-
-    });
-  });
-});
