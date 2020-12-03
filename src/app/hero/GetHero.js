6ec261a4388aceb085d41999991e5b1974851817
emethium <nikolas.serafini@gmail.com>
2020-12-03 11:23:46 -0300

    feat(app/hero, resolvers/mutations): added missing deleteHero mutation and other requested changes

--- src/app/hero/GetHero.js
+++ src/app/hero/GetHero.js
@@ -1,5 +1,7 @@
+const { buildError, buildSuccess } = require('../utils');
+
 const GetHero = ({
   heroRepository,
-}) => (heroId) => heroRepository.getOne(heroId);
+}) => (heroId) => heroRepository.getOne(heroId).bimap(buildError, buildSuccess);
 
 module.exports = GetHero;
